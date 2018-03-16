bfded056034f4a3762844eac2ff519b4effe6ae9
Yuki Zhang <765971698@qq.com>
2018-03-16 12:21:50 +0800

    refact: use svg instead of input

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import clx from 'classnames'
 
 class Checkbox extends React.Component {
   constructor (props) {
@@ -10,14 +11,25 @@ class Checkbox extends React.Component {
 
   render () {
     const { children, value } = this.props
+    const active = this.state.isChecked
+    const checkboxClass = clx({
+      'or-checkbox': true,
+      active
+    })
     return (
-      <div>
-        <input
-          id={`checkbox-${value}`}
-          type='checkbox'
-          checked={this.state.isChecked}
-          onChange={this.handleClick} />
-        <label htmlFor={`checkbox-${value}`}>{children}</label>
+      <div className={checkboxClass} value={value} onClick={this.handleClick}>
+        <div className='checkbox-icon'>
+          <svg
+            fill='#ffffff'
+            height='18'
+            viewBox='0 0 24 24'
+            width='18'
+            xmlns='http://www.w3.org/2000/svg'>
+            <path d='M0 0h24v24H0z' fill='none' />
+            <path d='M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z' />
+          </svg>
+        </div>
+        <div className='checkbox-content'>{children}</div>
       </div>
     )
   }
